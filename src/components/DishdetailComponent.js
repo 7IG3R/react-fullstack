e49ac799d62281da129c0467aa033b4955dc5ec0
Shakti Saurav <28942894+7IG3R@users.noreply.github.com>
2022-08-21 17:17:51 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,58 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
 
-class DishdetailComponent extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            
-        };
-
-    };
-    convertDate(date){
-        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        var date = date.split('T')
-        var date = date[0].split('-')
-        const month = monthNames[date[1]]
-        const day = date[2]
-        const year = date[0]
-        const res = '' + month + ' ' + day + ' ' + year
-        return res
-    }
-    render(){
-        if(this.props.dish != null){
-            const comments = this.props.dish.comments.map((comment) => {
-                return (
-                    <><br/>
-                    {comment.comment}<br/><br/>
-                    -- {comment.author} , {this.convertDate(comment.date)}<br/>
-                    </>
-                )
-            })
-            return(
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-12 col-md-5 m-1'>
-                            <Card>
-                                <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                                <CardBody>
-                                <CardTitle><b>{this.props.dish.name}</b></CardTitle>
-                                <CardText>{this.props.dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className='col-12 col-md-5 m-1'>
-                            <b>Comments</b>
-                            {comments}
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-        else{
-            
-        }
-    }
+function convertDate(date){
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    date = date.split('T')
+    date = date[0].split('-')
+    const month = monthNames[date[1]]
+    const day = date[2]
+    const year = date[0]
+    const res = '' + month + ' ' + day + ' ' + year
+    return res
 }
 
-export default DishdetailComponent;
+function RenderComments({comments}){
+    return comments.map((comment) => {
+        return (
+            <>
+            <br/>
+            {comment.comment}
+            <br/><br/>
+            -- {comment.author} , {convertDate(comment.date)}<br/>
+            </>
+        )
+    })
+}
+
+function RenderDish({dish}){
+    return(
+        <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+            <CardTitle><b>{dish.name}</b></CardTitle>
+            <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
+
+const DishDetail = ({dish}) => {
+    if(dish != null){
+        return(
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12 col-md-5 m-1'>
+                        <RenderDish dish = {dish}/>
+                    </div>
+                    <div className='col-12 col-md-5 m-1'>
+                        <b>Comments</b>
+                        <RenderComments comments = {dish.comments}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default DishDetail;
